f0fbcad5364ae1e48c1d487c8f65b42f7579af37
Vanderson Tonus <vandersontonus@gmail.com>
2021-04-02 08:26:10 +0100

    First NETCONF script

--- Testing.py
+++ Testing.py
@@ -1,4 +1,12 @@
 from ncclient import manager
 
-print("Testing Python")
-manager.connect(something="something")
+router = {"host": "52.30.30.218", "port": "830", "username": "VandersonKjdy7s3y", "password": "&SBJEwkwis98f7u&2"}
+
+with manager.connect(host=router["host"], port=router["port"], username=router["username"], password=router["password"], hostkey_verify=False) as m:
+ for capability in m.server_capabilities:
+  print('*' * 50)
+  print(capability)
+ m.close_session()
+
+
+
